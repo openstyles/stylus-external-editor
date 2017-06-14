20bbdb6f74bec4c879fb90814958bb869970a0b0
Jeremy Schomery <jeremy.schomery@yahoo.com>
2017-06-14 15:07:12 +0430

    first commit

---
+++ background.js
@@ -0,0 +1,82 @@
+/* globals Native */
+'use strict';
+
+function open ({onCreated, onChanged, onError}) {
+  function external (tmpdir) {
+    /* globals push, close */
+    var crypto = require('crypto');
+    var fs = require('fs');
+
+    var filename = require('path').join(tmpdir, 'stylus-' + crypto.randomBytes(4).readUInt32LE(0) + '.css');
+    fs.writeFile(filename, '/* this is a temporary style file */', e => {
+      if (e) {
+        push({
+          type: 'internal',
+          error: e.message
+        });
+        close();
+      }
+      else {
+        push({
+          method: 'file-created',
+          filename
+        });
+        fs.watchFile(filename, event => {
+          fs.readFile(filename, 'utf8', (e, data) => {
+            if (e) {
+              push({
+                type: 'file-read',
+                error: e.message
+              });
+            }
+            else {
+              push({
+                method: 'file-changed',
+                data,
+                event
+              });
+            }
+          });
+        });
+      }
+    });
+  }
+
+  var native = new Native();
+  native.on('error', onError);
+  native.on('response', resp => {
+    if (resp.method === 'file-created') {
+      onCreated(resp.filename);
+    }
+    else if (resp.method === 'file-changed') {
+      onChanged(resp);
+    }
+    else if (resp.error) {
+      onError(resp.error);
+    }
+    else {
+      console.error(resp);
+      throw Error('unsupported response');
+    }
+  });
+  native.post({
+    method: 'spec'
+  }).then(resp => {
+    const tmpdir = resp.tmpdir;
+    if (tmpdir) {
+      native.send({
+        permissions: ['crypto', 'fs', 'path'],
+        script: `(${external.toString()})('${tmpdir}')`
+      });
+    }
+    else {
+      throw Error('cannot find system\'s temporary directory');
+    }
+  }).catch(onError);
+}
+
+open({
+  onCreated: filename => console.log('temporary file is created', filename),
+  onChanged: resp => console.log('file changed', resp),
+  onError: e => console.error(e)
+});
---
+++ native.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var Native = function () {
+  this.channel = chrome.runtime.connectNative('com.add0n.stylus');
+  this.callbacks = {};
+  this.resolve = null;
+  this.reject = null;
+
+  this.channel.onDisconnect.addListener(() => {
+    const error = new Error('Unexpended exit of the native client');
+    if (this.reject) {
+      this.reject(error);
+    }
+    else {
+      this.emit('error', error);
+    }
+    this.resolve = this.reject = this.channel = null;
+  });
+  this.channel.onMessage.addListener(response => {
+    if (this.resolve) {
+      this.resolve(response);
+    }
+    else {
+      this.emit('response', response);
+    }
+    this.resolve = this.reject = null;
+  });
+};
+Native.prototype.on = function (name, callback) {
+  this.callbacks[name] = this.callbacks[name] || [];
+  this.callbacks[name].push(callback);
+};
+Native.prototype.emit = function (name, data) {
+  (this.callbacks[name] || []).forEach(c => c(data));
+};
+Native.prototype.post = function (request) {
+  if (!this.channel) {
+    return Promise.reject(new Error('there is no open channel'));
+  }
+  return new Promise((resolve, reject) => {
+    this.resolve = resolve;
+    this.reject = reject;
+    this.channel.postMessage(request);
+  });
+};
+Native.prototype.send = function (request) {
+  if (!this.channel) {
+    return Promise.reject(new Error('there is no open channel'));
+  }
+  this.channel.postMessage(request);
+};
+Native.prototype.kill = function () {
+  if (this.channel) {
+    this.channel.disconnect();
+  }
+  this.channel = null;
+};
